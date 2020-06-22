66ab586cb4b2b0be18756d1628263a11b6c5465c
Álvaro Prieto <alviprieto@hotmail.com>
2020-06-21 23:14:47 -0300

    Change DAL folder name to Data. Add services with interfaces

---
+++ segundoparcial-mtorres/Business/AnimeService.cs
@@ -0,0 +1,17 @@
+﻿using segundoparcial_mtorres.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace segundoparcial_mtorres.Business
+{
+    public class AnimeService
+    {
+        private ApplicationDbContext _context;
+
+        public AnimeService(ApplicationDbContext context) => _context = context;
+
+
+    }
+}
---
+++ segundoparcial-mtorres/Business/CategoryService.cs
@@ -0,0 +1,12 @@
+﻿using segundoparcial_mtorres.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace segundoparcial_mtorres.Business
+{
+    public class CategoryService : ICategoryService
+    {
+    }
+}
