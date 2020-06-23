d9fad88b0fe1b125d154d520e51c631ed71e3a10
Álvaro Prieto <alviprieto@hotmail.com>
2020-06-23 00:09:19 -0300

    Add paginated models

---
+++ segundoparcial-mtorres/Models/PaginatedRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace segundoparcial_mtorres.Models
+{
+    public class PaginatedRequest
+    {
+        public int? PageNumber { get; set; }
+        public int? PageSize { get; set; }
+        public string SortBy { get; set; }
+        public SortDirection SortDirection { get; set; }
+    }
+}
