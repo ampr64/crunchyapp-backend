03f2792dc06aa8cff6e97ffc681c88111911943a
Álvaro Prieto <alviprieto@hotmail.com>
2020-06-23 00:11:57 -0300

    Add ExpressionBuilder

---
+++ segundoparcial-mtorres/Helpers/ExpressionBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace segundoparcial_mtorres.Helpers
+{
+    public static class ExpressionBuilder
+    {
+        public static Expression<Func<T, object>> BuildPropertyAccessExpression<T>(string propertyName)
+        {
+            Expression<Func<T, object>> selector = null;
+            var entityType = typeof(T);
+            var parameter = Expression.Parameter(entityType, entityType.Name);
+            var body = Expression.PropertyOrField(parameter, propertyName);
+            if (body != null)
+            {
+                selector = Expression.Lambda<Func<T, object>>(body, parameter);
+            }
+
+            return selector;
+        }
+    }
+}
